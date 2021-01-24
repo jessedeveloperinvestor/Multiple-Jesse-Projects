d2d295f97a08290d5c2e38cc2084ac0b45b02ce3
Jesse <77866799+jessedeveloperinvestor@users.noreply.github.com>
2021-01-24 17:19:27 -0300

    Update Browser.py

--- Browser.py
+++ Browser.py
@@ -1,4 +1,14 @@
 import urllib.request, urllib.parse, urllib.error
-fhand= urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
-for line in fhand:
-    print(line.decode().strip())
+n = 0
+while n > -1:
+    try:
+        x = input("Hello, type in the link that you'd like to load\n")
+        fhand= urllib.request.urlopen(x)
+    except:
+        x = 'http://data.pr4e.org/romeo.txt'
+        print("This URL is wrong\nLoading" + x + '\n')
+    fhand= urllib.request.urlopen(x)
+    for line in fhand:
+        print(line.decode().strip())
+    print('\n')
+    n = n + 1
