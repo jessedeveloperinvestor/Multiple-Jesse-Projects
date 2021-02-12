7c9c5f2e357cd19736cd7ce2bc542190335632be
Jesse <77866799+jessedeveloperinvestor@users.noreply.github.com>
2021-02-11 21:06:46 -0300

    Add files via upload

---
+++ Decryptor.py
@@ -0,0 +1,22 @@
+#Jesse's Decriptor
+
+import time
+print("Input single encrypted characters multiple times, then type 'ok' and hit enter")
+#Decription
+o = list()
+while True:
+    z = input('Enter the message:\n')
+    if z=='a876':
+        o.append(' ')
+    if z=='242j':
+        o.append('a')
+    if z=='d03k' in z:
+        o.append('b')
+    if z=='1k6d' in z:
+        o.append('8')
+    if z=='p16x' in z:
+        o.append('9')
+    if z=='ok':
+        print(o)
+        time.sleep(30)
+        quit()
---
+++ Encryptor.py
@@ -0,0 +1,22 @@
+#Jesse's Encriptor
+
+import time
+print("Input single characters multiple times, then type 'ok' and hit enter")
+#Decription
+o = list()
+while True:
+    z = input('Enter the message:\n')
+    if z==' ':
+        o.append('a876')
+    if z=='a':
+        o.append('242j')
+    if z=='b' in z:
+        o.append('d03k')
+    if z=='8' in z:
+        o.append('1k6d')
+    if z=='9' in z:
+        o.append('p16x')
+    if z=='ok':
+        print(o)
+        time.sleep(30)
+        quit()
