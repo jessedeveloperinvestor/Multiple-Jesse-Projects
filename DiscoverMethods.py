de6aecb0578d41d02cb5df90203b70f223d12b5f
Jesse <77866799+jessedeveloperinvestor@users.noreply.github.com>
2021-03-19 23:47:37 -0300

    Update DiscoverMethods.py

--- DiscoverMethods.py
+++ DiscoverMethods.py
@@ -1,4 +1,6 @@
-y=''
-print(type(y))
-print('\n')
-print(dir(y))
+while True:
+	print("Hello, please type an example of an object that you'd like to know its methods")
+	y=input()
+	print(type(y))
+	print('\n')
+	print(dir(y))
