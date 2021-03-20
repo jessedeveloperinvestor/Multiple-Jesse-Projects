8a9a1f93d95c8e7f966a5a9b4a50d0da06e91d8f
Jesse <77866799+jessedeveloperinvestor@users.noreply.github.com>
2021-03-19 23:44:25 -0300

    Update Translator.py

--- Translator.py
+++ Translator.py
@@ -1,8 +1,12 @@
 #pip install translate
 from translate import Translator
-text='Hello World'
-i='english'
-o='pt-br'
-set = Translator(from_lang=i, to_lang=o)
-y=set.translate(text)
-print(y)
+print('Hello, write something in English and hit enter to get it translated')
+z=0
+while z<1:
+    text=input()
+    i='english'
+    o='pt-br'
+    set = Translator(from_lang=i, to_lang=o)
+    y=set.translate(text)
+    print(y)
+    z=z-1
