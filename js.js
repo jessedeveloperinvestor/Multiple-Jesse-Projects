16180ab787290026bcc81d714b710f37a8b54b85
Jesse <77866799+jessedeveloperinvestor@users.noreply.github.com>
2021-04-05 01:52:38 -0200

    Update js.js

--- js.js
+++ js.js
@@ -1,1 +1,19 @@
+//Hi there
+/*
+this is a comment
+*/
 console.log('Are you ok?')
+var y='hi'
+var n=0
+var ax=y
+var $x=n+5
+var _x='ok'
+if(y=='hello'){
+	console.log('great');
+}
+else if(y=='hi'){
+	console.log($x);
+}
+else{
+	console.log(_x);
+}
