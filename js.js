c28cc658e214d37e4e60f6066d9f82c231ec280a
Jesse <77866799+jessedeveloperinvestor@users.noreply.github.com>
2021-04-05 03:04:35 -0200

    Update js.js

--- js.js
+++ js.js
@@ -17,3 +17,43 @@ else if(y=='hi'){
 else{
 	console.log(_x);
 }
+//conditional switch:
+var day='sun';
+switch(day) {
+    case 'mon':
+break;
+    case 'tue':
+break;
+    case 'wed':
+break;
+    case 'thu':
+break;
+    case 'fri':
+break;
+    case 'sat':
+break;
+default: 'sun';
+break;
+}
+console.log(day)
+
+//
+
+function exibir(){
+ var mes = parseInt(document.getElementById("numero").value);
+ 
+ switch(mes){
+  case 1:  alert("Janeiro");
+  case 2:  alert("Fevereiro");
+  case 3:  alert("Março");
+  case 4:  alert("Abril");
+  case 5:  alert("Maio");
+  case 6:  alert("Junho");
+  case 7:  alert("Julho");
+  case 8:  alert("Agosto");
+  case 9:  alert("Setembro");
+  case 10: alert("Outubro");
+  case 11: alert("Novembro");
+  case 12: alert("Dezembro");
+ }
+}
