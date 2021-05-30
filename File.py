111266a36e86f73a4cb02b26f6c8e2af7367d995
Jesse <77866799+jessedeveloperinvestor@users.noreply.github.com>
2021-05-30 18:12:58 -0200

    Update File.py

--- File.py
+++ File.py
@@ -3,13 +3,30 @@ with open('file.txt', 'w') as file:
 	file.write("Python and Jesse built this file!")
 with open('file.txt', 'a') as file:
 	file.write('\nJesse\n'+str(y)+'\n'+str(y))
+inf=[]
+with open('file.data', 'r') as file:
+	for row in file.readlines():
+		inf.append(row.split(','))
+
 data=[]
 with open('file.txt', 'r') as file:
 		a=file.read()+'\n'
-
 with open('file.data', 'w') as file:
 	file.write(str(y))
 with open('file.data', 'r') as file:
 	for row in file.readlines():
 		data.append(row.split(','))
-	print(a+str(data))
+
+aa=inf[0][0]+inf[0][1]+inf[0][2]
+ab=str(aa)
+
+a0=data[0][0]+data[0][1]+data[0][2]
+a1='       Data from .TXT file:\n\n'+str(a0)+'\n\n----------------------------------------------------------\n       Data from .DATA file:\n\n'+ab
+a2=a1.translate({ord(i): None for i in "["})
+a3=a2.translate({ord(i): None for i in "]"})
+a4=a3.translate({ord(i): None for i in ","})
+a5=a4.translate({ord(i): None for i in "{"})
+a6=a5.translate({ord(i): None for i in "}"})
+a7=a6.translate({ord(i): None for i in ":"})
+a8=a7.translate({ord(i): '\n' for i in "'"})
+print(a8)
