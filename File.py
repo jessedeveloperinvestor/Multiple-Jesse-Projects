f68d1f1ed3f46acda09c90c893e46eb93c68f3a8
Jesse <77866799+jessedeveloperinvestor@users.noreply.github.com>
2021-05-30 17:48:58 -0200

    Delete File.py

--- File.py
+++
@@ -1,7 +0,0 @@
-y={0:{'Jesse Leite',23,'Best computer Engineer'},1:{'Lana Rhoades',22,'Best pleasure professional'}}
-with open('file.txt', 'w') as file:
-	file.write("Python and Jesse built this file!")
-with open('file.txt', 'a') as file:
-	file.write('\nJesse\n'+str(y))
-with open('file.txt', 'r') as file:
-	print(file.read())
