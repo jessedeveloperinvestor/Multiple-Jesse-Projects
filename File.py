568edd6eb04ca0521efb1a7d43f841a74720847f
Jesse <77866799+jessedeveloperinvestor@users.noreply.github.com>
2021-05-30 17:23:46 -0200

    Update File.py

--- File.py
+++ File.py
@@ -1,6 +1,7 @@
+y={0:{'Jesse Leite',23,'Best computer Engineer'},1:{'Lana Rhoades',22,'Best pleasure professional'}}
 with open('file.txt', 'w') as file:
 	file.write("Python and Jesse built this file!")
 with open('file.txt', 'a') as file:
-	file.write('\nJesse')
+	file.write('\nJesse\n'+str(y))
 with open('file.txt', 'r') as file:
 	print(file.read())
