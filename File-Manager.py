892dc2ae377679de966639638eacc4746a5c4e53
Jesse <77866799+jessedeveloperinvestor@users.noreply.github.com>
2021-08-29 13:10:42 -0300

    wtf

---
+++ File-Manager.py
@@ -0,0 +1,9 @@
+import os
+print('Iterate over dirs and files:')
+path = './'
+for root, dirs, files in os.walk(path):
+	print(root)
+	for _dir in dirs:
+		print(_dir)
+	for _file in files:
+		print(_file)
